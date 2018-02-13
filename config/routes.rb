72efcd256b4875e609340f5052dd4b44b6b4b481
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-02-13 16:54:14 +0800

    add top 5star of restaurant in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :restaurants do 
+    collection do 
+      get 'top', to: "restaurants#top"
+    end
+  end
   get 'pages/contact'
-
   get 'pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
